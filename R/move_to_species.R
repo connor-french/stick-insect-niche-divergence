ed0dd4220234c34ae0825dd506cf2d0c012770b9
Connor French <confrench33@gmail.com>
2020-03-19 14:12:46 -0400

    finished refactoring for reproducibility

--- R/move_to_species.R
+++ R/move_to_species.R
@@ -2,26 +2,36 @@
 # you need a path to a directory that contains the files you want to move (in_path),
 # an output path that contains the directory you want to move the files to (out_path),
 # and a string pattern that identifies the files (e.g. "ahor" is on all file names attributed to species from Argosarchus horridus)
-
+# the string pattern just needs to be a short string contained. I append a regex expression to make it only search for files.
+library(fs)
 move_to_species <- function(in_path, out_path, pattern) {
   
   # check if subfolder exists. If not, make it
   if (!dir.exists(out_path)) {
     dir.create(out_path)
-  } else
+    
+    } else
     print("Directory already there.")
   
   # list all files in the input directory with their full paths
   in_files_full <-
     list.files(in_path, pattern = pattern, full.names = TRUE)
   
-  # list all files in the input directory with only the file names
-  in_files <- list.files(in_path, pattern = pattern)
-  
-  # append the file names to the out_path
-  out_files <- file.path(out_path, in_files)
-  
-  # move the files
-  file.rename(in_files_full, out_files)
+  # files without their full paths
+  if (
+    fs::dir_ls(in_path, type = "file", regexp = pattern) %>% 
+      basename() %>% 
+      length() > 1
+  ) {
+    in_files <- fs::dir_ls(in_path, type = "file", regexp = pattern) %>% 
+      basename()
+    
+    # append the file names to the out_path
+    out_files <- file.path(out_path, in_files)
+    
+    # move the files
+    file.rename(in_files_full, out_files)
+    
+  } else print("No files to move.")
   
 }
--- R/total_climate_pca_plot.R
+++ R/total_climate_pca_plot.R
@@ -13,9 +13,10 @@ total_climate_pca_plot <- function(bg_env, locs, genus, species) {
     as_tibble() %>% 
     mutate(reproductive_mode = locs$reproductive_mode)
   
-  total_df <- dplyr::bind_rows(env_df, sp_df)
+  total_df <- dplyr::bind_rows(env_df, sp_df) %>% 
+    na.omit()
   
-  pca_df <- total_df %>% dplyr::select(-reproductive_mode)
+  pca_df <- total_df %>% dplyr::select(-reproductive_mode) 
   
   env_pca <- prcomp(pca_df, center = TRUE, scale. = TRUE)
   
@@ -23,11 +24,23 @@ total_climate_pca_plot <- function(bg_env, locs, genus, species) {
     mutate(reproductive_mode = total_df$reproductive_mode)
   
   ggplot() +
-    geom_hex(data = pca_out_df %>% filter(reproductive_mode == "none"), aes(x = PC1, y = PC2, fill = ..count..)) +
+    geom_hex(data = pca_out_df %>% filter(reproductive_mode == "none"),
+             aes(x = PC1, y = PC2, fill = ..count..)) +
     scale_fill_gradient(low = "lightgray", high = "black") +
     labs(fill = "Count") +
     ggnewscale::new_scale_fill() +
-    geom_point(data = pca_out_df %>% filter(reproductive_mode != "none"), aes(x = PC1, y = PC2, fill = reproductive_mode, color = reproductive_mode), alpha = 0.8, size = 3, pch = 21) +
+    geom_point(
+      data = pca_out_df %>% filter(reproductive_mode != "none"),
+      aes(
+        x = PC1,
+        y = PC2,
+        fill = reproductive_mode,
+        color = reproductive_mode
+      ),
+      alpha = 0.8,
+      size = 3,
+      pch = 21
+    ) +
     scale_fill_viridis_d(option = "inferno") +
     scale_color_manual(values = c("white", "black"), guide = FALSE) +
     labs(title = paste("Total climate space PCA for", genus, species),
